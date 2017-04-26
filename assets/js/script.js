5382226c044b799fdd33f765194ad12e813e56bf
Thu Nguyen <tmnguyen@berkeley.edu>
2017-04-25 18:25:59 -0700

    edits

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,23 @@
 $(document).ready(function() {
 
-  // jQuery functions: (1) .click(), (2) .offset()
+  // jQuery functions: (1) .click(), (2) .offset(), (3) .attr()
   $('a').click(function() {
     $('html, body').animate({
-      scrollTop: $( $.attr(this, 'href')).offset().top
+      scrollTop: $($.attr(this, 'href')).offset().top
     }, 300);
     return false;
-  })
-  
+  });
+
+// jQuery functions: (4) .hover() (5) .css()
+  $('a').hover(function() {
+    $('.sidebar-item', this).css('color', 'teal');
+  }).mouseout(function() {
+    $('.sidebar-item', this).css('color', 'white');
+  });
+
+  $('#name-text').hover(function() {
+    $('#title-subtext').fadeIn('slow');
+  });
+
+
 })
