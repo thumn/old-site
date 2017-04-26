19a353d1245eea652b73999ee44c2fc2723daa52
Thu Nguyen <tmnguyen@berkeley.edu>
2017-04-25 17:11:30 -0700

    edits to experience

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
+
+  // jQuery functions: (1) .click(), (2) .offset()
   $('a').click(function() {
     $('html, body').animate({
       scrollTop: $( $.attr(this, 'href')).offset().top
     }, 300);
     return false;
   })
+  
 })
