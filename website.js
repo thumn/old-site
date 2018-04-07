d172863004f43ec91db2b7d439ac0a8660fc7e25
Thu Nguyen <tmnguyen@berkeley.edu>
2018-04-07 02:06:16 -0700

    Added fading feature to albums

--- website.js
+++ website.js
@@ -4,12 +4,16 @@ $(window).load(function(){
 
 $(document).ready(function(){
   $('#link-max').click(function() {
+      $('#photo-emily').fadeOut(1000);
       $('#photo-emily').css('display', 'none');
+      $('#photo-max').fadeIn(1000);
       $('#photo-max').css('display', 'block');
   });
 
   $('#link-emily').click(function() {
+      $('#photo-max').fadeOut(1000);
       $('#photo-max').css('display', 'none');
+      $('#photo-emily').fadeIn(1000);
       $('#photo-emily').css('display', 'block');
   });
 });
