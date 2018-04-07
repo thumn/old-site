72d8f1225a427d2715a85a3de57fdcf68dd80d6e
Thu Nguyen <tmnguyen@berkeley.edu>
2018-04-07 14:05:44 -0700

    bold text in photo side bar if click on link

--- website.js
+++ website.js
@@ -4,6 +4,8 @@ $(window).load(function(){
 
 $(document).ready(function(){
   $('#link-max').click(function() {
+      $('#photo-list-item-max').css('font-weight', 'bold');
+      $('#photo-list-item-emily').css('font-weight', 'normal');
       $('#photo-emily').fadeOut(1000);
       $('#photo-emily').css('display', 'none');
       $('#photo-max').fadeIn(1000);
@@ -11,6 +13,8 @@ $(document).ready(function(){
   });
 
   $('#link-emily').click(function() {
+      $('#photo-list-item-emily').css('font-weight', 'bold');
+      $('#photo-list-item-max').css('font-weight', 'normal');
       $('#photo-max').fadeOut(1000);
       $('#photo-max').css('display', 'none');
       $('#photo-emily').fadeIn(1000);
