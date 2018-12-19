c70ad418c32afcffc7d2a9b86c859cc06b23de9b
Thu Nguyen <tmnguyen@berkeley.edu>
2018-12-18 21:14:56 -0800

    finally some updates

--- website.js
+++ website.js
@@ -6,8 +6,14 @@ $(document).ready(function(){
   $('#link-max').click(function() {
       $('#photo-list-item-max').css('font-weight', 'bold');
       $('#photo-list-item-emily').css('font-weight', 'normal');
+      $('#photo-list-item-ikea').css('font-weight', 'normal');
+      $('#photo-list-item-fa18').css('font-weight', 'normal');
       $('#photo-emily').fadeOut(1000);
       $('#photo-emily').css('display', 'none');
+      $('#photo-ikea').fadeOut(1000);
+      $('#photo-ikea').css('display', 'none');
+      $('#photo-fa18').fadeOut(1000);
+      $('#photo-fa18').css('display', 'none');
       $('#photo-max').fadeIn(1000);
       $('#photo-max').css('display', 'block');
   });
@@ -15,9 +21,45 @@ $(document).ready(function(){
   $('#link-emily').click(function() {
       $('#photo-list-item-emily').css('font-weight', 'bold');
       $('#photo-list-item-max').css('font-weight', 'normal');
+      $('#photo-list-item-ikea').css('font-weight', 'normal');
+      $('#photo-list-item-fa18').css('font-weight', 'normal');
       $('#photo-max').fadeOut(1000);
       $('#photo-max').css('display', 'none');
+      $('#photo-ikea').fadeOut(1000);
+      $('#photo-ikea').css('display', 'none');
+      $('#photo-fa18').fadeOut(1000);
+      $('#photo-fa18').css('display', 'none');
       $('#photo-emily').fadeIn(1000);
       $('#photo-emily').css('display', 'block');
+  });
+
+  $('#link-ikea').click(function() {
+      $('#photo-list-item-ikea').css('font-weight', 'bold');
+      $('#photo-list-item-max').css('font-weight', 'normal');
+      $('#photo-list-item-emily').css('font-weight', 'normal');
+      $('#photo-list-item-fa18').css('font-weight', 'normal');
+      $('#photo-max').fadeOut(1000);
+      $('#photo-max').css('display', 'none');
+      $('#photo-emily').fadeOut(1000);
+      $('#photo-emily').css('display', 'none');
+      $('#photo-fa18').fadeOut(1000);
+      $('#photo-fa18').css('display', 'none');
+      $('#photo-ikea').fadeIn(1000);
+      $('#photo-ikea').css('display', 'block');
+  });
+
+  $('#link-fa18').click(function() {
+      $('#photo-list-item-fa18').css('font-weight', 'bold');
+      $('#photo-list-item-ikea').css('font-weight', 'normal');
+      $('#photo-list-item-max').css('font-weight', 'normal');
+      $('#photo-list-item-emily').css('font-weight', 'normal');
+      $('#photo-max').fadeOut(1000);
+      $('#photo-max').css('display', 'none');
+      $('#photo-emily').fadeOut(1000);
+      $('#photo-emily').css('display', 'none');
+      $('#photo-ikea').fadeOut(1000);
+      $('#photo-ikea').css('display', 'none');
+      $('#photo-fa18').fadeIn(1000);
+      $('#photo-fa18').css('display', 'block');
   });
 });
